0172a6510014d403e63bf705e6bf0ae3313b1a25
Eyt-Lev <ecraft452@gmail.com>
2023-04-17 17:34:45 +0300

    Organize, Music bug fix and level 3 (without the explain, probably not done).

--- src/components/watering/dough.py
+++ src/components/watering/dough.py
@@ -4,7 +4,6 @@ from src.global_state import GlobalState
 from src.services.visualization_service import VisualizationService
 from src.tools import is_img_mask_collide_with_mouse
 from src.services.music_service import MusicService
-from src.game_status import GameStatus
 
 
 class Dough(pygame.sprite.Sprite):
